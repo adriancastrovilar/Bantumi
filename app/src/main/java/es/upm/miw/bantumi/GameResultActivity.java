390f802f9d1d4339522f4a1b147c29a56bdc2eb6
adriancastrovilar <adrian.castrov@alumnos.upm.es>
2023-10-26 22:07:43 +0200

    Corregidas imperfecciones del GameResultActivity

--- app/src/main/java/es/upm/miw/bantumi/GameResultActivity.java
+++ app/src/main/java/es/upm/miw/bantumi/GameResultActivity.java
@@ -2,7 +2,6 @@ package es.upm.miw.bantumi;
 
 import android.os.Bundle;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -19,16 +18,13 @@ public class GameResultActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_result_activity);
-        final ActionBar actionBar = getSupportActionBar();
-        assert actionBar != null;
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setHomeAsUpIndicator(android.R.drawable.ic_menu_revert);
-        gameResultViewModel = new ViewModelProvider(this).get(GameResultViewModel.class);
-        RecyclerView rv = findViewById(R.id.recycler_view_game_result);
 
         final GameResultListAdapter gameResultListAdapter = new GameResultListAdapter(new GameResultListAdapter.GameResultDiff());
+        RecyclerView rv = findViewById(R.id.recycler_view_game_result);
         rv.setAdapter(gameResultListAdapter);
         rv.setLayoutManager(new LinearLayoutManager(this));
+
+        gameResultViewModel = new ViewModelProvider(this).get(GameResultViewModel.class);
         gameResultViewModel.getBestResults().observe(this, gameResultListAdapter::submitList);
     }
 
